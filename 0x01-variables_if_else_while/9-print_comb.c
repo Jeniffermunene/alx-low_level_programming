046587b353d90f8c11e5c280095e74c407b90480
jeniffermunene <jeniffermunene199@gmail.com>
2022-03-13 13:04:45 -0700

    add task 9

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ *  main - print out numbers in sigle digit separated
+ *  by comma and space using putchar() function
+ *  Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int n = 48;
+
+	while (n <= 57)
+	{
+		putchar(n);
+		if (n != 57)
+		{
+		putchar(',');
+		putchar(' ');
+		}
+		n++;
+	}
+	putchar('\n');
+	return (0);
+}
+			
